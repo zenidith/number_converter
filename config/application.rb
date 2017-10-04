bcb8c5eda9b88e279f842ca1557508b19b3993d3
Patrick Smith <patrick@burntcaramel.com>
2017-10-05 09:38:52 +1100

    Add Rspec

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,9 @@ module NumberConverter3000
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
